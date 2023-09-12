a3a20f3cc5b7b70b295803e4e1b92bc5bba63850
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-12 11:30:22 -0700

    added routes to my App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
@@ -9,7 +9,20 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/" element={<AboutMe />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
